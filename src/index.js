8c6ac777f7e1242825b04b82acfed5c3493e772d
antonioadv08 <antonioadv08@gmail.com>
2020-07-21 02:16:01 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const app=require("./app")
+
+async function main(){
+   await app.listen(3000)
+    console.log("Server is running")
+}
+
+
+main()
+
+
+// app.listen(3000,()=>{
+//     console.log("Server on port 3000")
+// })
